b762482eb84073ce51d9e6de63f303c965e3f572
Srishty Mangutte <srishtymangutte@gmail.com>
2023-01-26 23:16:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 
+window.onload=()=>
+{
+    
+    
 const btna = document.getElementById("a");
+const btnb = document.getElementById("b");
+const btnc = document.getElementById("c");
 
-const prom =()=>{
+btna.addEventListener("click",()=>{
+    window.prompt("Welcome!");
+});
+
+
+btnb.addEventListener("click", ()=>{
+    //let target= document.querySelector('#first');
+    let bod = document.body;
+    
+    if(bod.style.backgroundColor=='grey'){
+        bod.style.backgroundColor = '#2ab8a5';
+    }
+    else{
+        bod.style.backgroundColor = 'grey';
+        //window.alert('changing back')
+    }
+});
+
+
+btnc.addEventListener("click", ()=>{
+    setTimeout(hdng, 5000);
+});
+
+
+
+}
+/* function prom(){
     window.prompt("Welcome!");
 }
+const changebg =()=>{
+    window.prompt("Welcome!");
+}
+const hdng =()=>{
+    window.prompt("Welcome!");
+}
+
+*/
